bfa1a6d14689341ebc2b731ffe057128e72ceaf8
Aaron Schreck <schreck.aaron@gmail.com>
2018-03-14 12:40:37 -0600

    update 404 handling

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ app.use(express.static(__dirname + '/public'));
 let aCount = 0;
 let sCount = 0;
 
-app.get('/favicon.ico', function(req, res) {
-    res.status(204);
-})
-
 app.post("/a-increment", (req, res) => {
     aCount += 1;
     return res.send(JSON.stringify({aristotleCount: aCount}));
@@ -21,6 +17,10 @@ app.post("/a-increment", (req, res) => {
 app.post("/s-increment", (req, res) => {
     sCount += 1;
     return res.send(JSON.stringify({shaqCount: sCount}));
+})
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
 })
 
 app.listen(port);
